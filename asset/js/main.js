bacd380515a0f379531562855e07c8b4b9db44d2
AbdelhamidAyadi <Abdelhamid2002ayadi@gmail.com>
2022-03-14 19:03:19 +0100

    Final commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,26 +1,22 @@
-for(var i = 1 ; i< 100 ; i++){
-    document.getElementById("numbersContainer").innerHTML += `<span class=" box">${i}</span>`;
-
+for (var i = 1; i < 101; i++) {
     
-    
+   
 
-    if( i % 3 == 0 && i % 5 == 0 ) {
-      
-        document.getElementById("numbersContainer").innerHTML += `<span class="red box">Buzz</span>`;
-    }
-    else if( i % 3 == 0){ 
-       
+    if (i % 3 == 0 && i % 5 == 0) {
+
+        document.getElementById("numbersContainer").innerHTML += `<span class="red box">FizzBuzz</span>`;
+    } else if (i % 3 == 0) {
+
         document.getElementById("numbersContainer").innerHTML += `<span class="green box">Fizz</span>`;
 
-    }else if (i % 5 == 0){
-        
+    } else if (i % 5 == 0) {
+
         document.getElementById("numbersContainer").innerHTML += `<span class="yellow box">Buzz</span>`;
 
     }
-   
-   
-
-
-
+    else{
+        document.getElementById("numbersContainer").innerHTML += `<span class="box">${i}</span>`;
+    }
 
 }
+
